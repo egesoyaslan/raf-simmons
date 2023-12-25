c9247bb6b66c81992eb16460abf7f5e1b7431c0a
egesoyaslan <ege.soyaslan@yahoo.com>
2023-12-26 00:57:42 +0300

    Add script.js Delete arrowdown2.png Edit ss02.html, style.css and script.js
    Replaced the arrow with "ALBUM" text. Clicking on it smoothly scrolls
    to the models section.

---
+++ script.js
@@ -0,0 +1,26 @@
+const arrow = document.querySelector(`.arrow`);
+
+arrow.addEventListener(`click`, (e) => {
+    const target = (e.target).className;
+
+    if (target === `arrow`) {
+        const models = document.querySelector(`.models`);
+        models.scrollIntoView({behavior: `smooth`});
+    }
+});
+
+arrow.addEventListener(`mouseover`, (e) => {
+    const target = e.target;
+
+    if (target.className === `arrow`) {
+        target.style.color = `gainsboro`;
+    }
+});
+
+arrow.addEventListener(`mouseout`, (e) => {
+    const target = e.target;
+
+    if (target.className === `arrow`) {
+        target.style.color = `whitesmoke`;
+    }
+});
